9ea119a8250798a4162d34f8a4092d9f3baec8d1
Patryk Kubacki <patryk.kubacki@outlook.com>
2019-10-19 18:09:27 +0200

    initial project archimedes

---
+++ archimedes/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Archimedes</title>
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <div class="content">
+            <div>
+                Archimedes
+            </div>
+        </div>
+    </body>
+</html>
