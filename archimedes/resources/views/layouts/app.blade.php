f18b0c2c7957df06794466c43e22d98210edaed2
Patryk Kubacki <patryk.kubacki@outlook.com>
2019-10-20 17:11:40 +0200

    add auth

--- archimedes/resources/views/layouts/app.blade.php
+++ archimedes/resources/views/layouts/app.blade.php
@@ -11,15 +11,55 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
     header {
-        margin-bottom:80px;
+      margin-bottom:15px; 
+    }
+    /* .navbar-nav {
+         flex-direction: row;
+    } */
+    .nav-item {
+        margin-right:10px;
     }
     </style>
   </head>
   <body>
-
+  
     <header>
-        <nav class="navbar navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="#">Archimedes</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a class="navbar-brand" href="/">Archimedes</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    @guest
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Zaloguj się</a>
+                        </li>
+                        @if (Route::has('register'))
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('register') }}">Zarejestruj się</a>
+                            </li>
+                        @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                     @endguest
+                </ul>
+            </div>
         </nav>
     </header>
 
